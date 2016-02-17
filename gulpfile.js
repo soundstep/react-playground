ee6609d1e8fd798ea5a6c25f11c6cef13c54635f
soundstep <romu@soundstep.com>
2016-02-17 14:56:16 +0000

    Cleanup.

--- gulpfile.js
+++ gulpfile.js
@@ -56,36 +56,7 @@ var browserifyTask = function () {
     });
 
     return es.merge.apply(null, tasks);
-
-
-    // var count = 0;
-    //
-    // var bundler = browserify({
-    //     extension: ['.js'],
-    //     entries: ['lesson-1/src/app.js'],
-    //     debug: true
-    // });
-    //
-    // var rebundle = function () {
-    //     var start = Date.now();
-    //     console.log(count + ': Building app...');
-    //     bundler
-    //         .transform(babelify, {presets: ['es2015', 'react']})
-    //         .bundle()
-    //         .on('error', gutil.log)
-    //         .pipe(source('main.js'))
-    //         .pipe(gulp.dest('lesson-1/js/'))
-    //         .pipe(notify(function () {
-    //             console.log(count++ + ': App built in ' + (Date.now() - start) + 'ms');
-    //         }));
-    // };
-    //
-    // bundler = watchify(bundler);
-    // bundler.on('update', rebundle);
-    //
-    // rebundle();
 };
 
 gulp.task('build', browserifyTask);
-
 gulp.task('default', ['build']);
